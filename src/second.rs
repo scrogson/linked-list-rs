f511eb6f0f3bd11fbac878729b6231c6ddfd9b46
Sonny Scroggin <sonny@scrogg.in>
2018-10-07 13:08:46 -0500

    peek_mut

--- src/second.rs
+++ src/second.rs
@@ -34,6 +34,10 @@ impl<T> List<T> {
     pub fn peek(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem)
     }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -76,11 +80,13 @@ mod tests {
     fn peek() {
         let mut list = List::new();
         assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
 
         list.push(1);
         list.push(2);
         list.push(3);
 
         assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
     }
 }
